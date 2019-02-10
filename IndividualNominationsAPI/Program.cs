7cbd8a985b9965876de4196dff8f1b2cb2c757e3
Temitayo Fagbami <temitayo.fagbami@gmail.com>
2019-02-09 21:26:53 -0800

    add dbcontext and swagger services

--- IndividualNominationsAPI/Program.cs
+++ IndividualNominationsAPI/Program.cs
@@ -6,15 +6,33 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using IndividualNominationCatalogAPI.Data;
 
-namespace IndividualNominationsAPI
+namespace IndividualNominationCatalogAPI
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            //i added host variable, removed the run funtion from host builder
+            //did this to first have db seed first before running the service is kicked of at Main
+            var host = CreateWebHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                var context =
+
+                      services.GetRequiredService<IndividualNominationCatalogContext>();
+
+                // IndividualNominationCatalogSeed.SeedAsync(context).Wait();
+            }
+
+            //db has been seeded, now run
+            host.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
@@ -22,3 +40,4 @@ namespace IndividualNominationsAPI
                 .UseStartup<Startup>();
     }
 }
+
--- IndividualNominationsAPI/Startup.cs
+++ IndividualNominationsAPI/Startup.cs
@@ -5,15 +5,26 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
-namespace IndividualNominationsAPI
+namespace IndividualNominationCatalogAPI
 {
+    //Startup.cs- when API service starts,  Startup.cs get triggered first
+    //i need to set up what is needed for the API to run successfully
     public class Startup
     {
+
+        //startup ctor kicks off the API service by loading service configurations
+
+        //steps for this:
+        //appsetting.json contains connections strings needed by API service
+        //configuration grabs connection strings from appsettings.json
+        //configuration is inject in startup ctor
+        //startup ctor -reads the configurations
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -25,6 +36,52 @@ namespace IndividualNominationsAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            //database
+
+            //get connection string from configuration
+
+            var dbConnectionString = Configuration["ConnectionString"];
+
+
+            //build db by adding DBContext of type  IndividualNominationCatalogContext
+            //pass the db connectionstring from configuration to options
+            services.AddDbContext<IndividualNominationCatalogContext>(options => options.UseSqlServer(dbConnectionString));
+
+
+
+            //cannot call seed here, async issue, db might not be ready to take seed
+            //call in Progam.cs in main, it runs after startup
+
+
+            // Add framework services.
+            //add swagger
+            services.AddSwaggerGen(options =>
+
+            {
+
+                options.DescribeAllEnumsAsStrings();
+
+                options.SwaggerDoc("v1",
+
+                    new Swashbuckle.AspNetCore.Swagger.Info
+
+                    {
+
+                        Title = "RewardsRecognition - Individual Nomination Catalog HTTP API",
+
+                        Version = "v1",
+
+                        Description = "The Individual Nomination Microservice HTTP API. This is a Data-Driven/CRUD microservice API",
+
+                        TermsOfService = "Terms Of Service"
+
+                    });
+
+
+
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -34,6 +91,17 @@ namespace IndividualNominationsAPI
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            //configure swagger
+            app.UseSwagger()
+
+               .UseSwaggerUI(c =>
+
+               {
+
+                   c.SwaggerEndpoint($"/swagger/v1/swagger.json", "Individaul Nomination API V1");
+
+               });
 
             app.UseMvc();
         }
